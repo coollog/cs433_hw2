f30c76e1e61997c3bfbab24350a68096aad2ec3f
Qingyang Chen <qingyang.chen@gmail.com>
2016-02-10 20:07:36 -0500

    working

---
+++ PingClient.java
@@ -0,0 +1,96 @@
+// PingClient.java
+// usage: java PingClient host port passwd
+import java.io.*;
+import java.net.*;
+
+public class PingClient {
+  private static int SEQNUM = 0;
+  private static String PASSWD;
+
+  private static long RTT_MIN = Long.MAX_VALUE;
+  private static long RTT_MAX = Long.MIN_VALUE;
+  private static int RTT_N = 0;
+  private static long RTT_SUM = 0;
+  private static int LOSS_COUNT = 0;
+  private static final int PING_COUNT = 10;
+
+  public static void main(String[] args) throws Exception {
+    // Get command line argument.
+    if (args.length != 3) {
+      System.out.println("usage: java PingClient host port passwd");
+      return;
+    }
+
+    String serverName = args[0];
+    int serverPort = Integer.parseInt(args[1]);
+    PASSWD = args[2];
+
+    InetAddress serverIPAddress = InetAddress.getByName(serverName);
+
+    // Create socket.
+    DatagramSocket clientSocket = new DatagramSocket();
+
+    // Ping the server.
+    for (int i = 0; i < PING_COUNT; i ++) {
+      pingServer(clientSocket, serverIPAddress, serverPort);
+      Thread.sleep(1000);
+      SEQNUM ++;
+    }
+
+    // Output the statistics.
+    System.out.format("RTT min/avg/max: %d/%d/%d\n",
+      RTT_MIN, RTT_SUM/RTT_N, RTT_MAX);
+    System.out.format("Loss rate: %d%%\n", LOSS_COUNT * 100 / PING_COUNT);
+
+    // close client socket
+    clientSocket.close();
+
+  } // end of main
+
+  private static void pingServer(DatagramSocket clientSocket,
+                                 InetAddress serverIPAddress,
+                                 int serverPort) throws Exception {
+    // Build message:
+    // PING sequence_number client_send_time passwd CRLF
+    String seqNum = String.format("%02d", SEQNUM);
+    long sendTime = System.currentTimeMillis();
+    String msg = "PING " + SEQNUM + " " + sendTime + " " + PASSWD + " \r\n";
+
+    // Construct and send datagram.
+    byte[] sendData = msg.getBytes();
+    DatagramPacket sendPacket = new DatagramPacket(sendData,
+                                                   sendData.length,
+                                                   serverIPAddress,
+                                                   serverPort);
+    clientSocket.send(sendPacket);
+
+    // Receive datagram.
+    byte[] receiveData = new byte[1024];
+    DatagramPacket receivePacket = new DatagramPacket(receiveData,
+          receiveData.length);
+    clientSocket.setSoTimeout(1000);
+    try {
+      clientSocket.receive(receivePacket);
+    } catch (SocketTimeoutException e) {
+      System.out.println("Server response timeout: " + seqNum + ".");
+      LOSS_COUNT ++;
+      return;
+    }
+
+    // RTT logging.
+    long rtt = System.currentTimeMillis() - sendTime;
+    if (rtt < RTT_MIN) {
+      RTT_MIN = rtt;
+    }
+    if (rtt > RTT_MAX) {
+      RTT_MAX = rtt;
+    }
+    RTT_SUM += rtt;
+    RTT_N ++;
+
+    // Print output.
+    String sentenceFromServer = new String(receivePacket.getData());
+    System.out.println("From Server: " + sentenceFromServer);
+  }
+
+} // end of UDPClient
---
+++ PingServer.java
@@ -0,0 +1,183 @@
+// PingServer.java
+// usage: java PingServer port passwd [-delay delay] [-loss loss]
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+/*
+ * Server to process ping requests over UDP.
+ */
+
+public class PingServer {
+  private static double LOSS_RATE = 0.3;
+  private static int AVERAGE_DELAY = 100; // milliseconds
+  private static int PORT;
+  private static String PASSWD;
+
+  public static void main(String[] args) throws Exception {
+    if (!processArgs(args)) {
+      printUsage();
+      return;
+    }
+
+    // Create random number generator for use in simulating
+    // packet loss and network delay.
+    Random random = new Random();
+
+    // Create a datagram socket for receiving and sending
+    // UDP packets through the port specified on the
+    // command line.
+    DatagramSocket socket = new DatagramSocket(PORT);
+
+    // Processing loop.
+    while (true) {
+
+      // Create a datagram packet to hold incomming UDP packet.
+      DatagramPacket
+        request = new DatagramPacket(new byte[1024], 1024);
+
+      // Block until receives a UDP packet.
+      socket.receive(request);
+
+      // Print the received data, for debugging
+      printData(request);
+
+      // Decide whether to reply, or simulate packet loss.
+      if (random.nextDouble() < LOSS_RATE) {
+        System.out.println(" Reply not sent.");
+        continue;
+      }
+
+      // Simulate prorogation delay.
+      Thread.sleep((int) (random.nextDouble() * 2 * AVERAGE_DELAY));
+
+      // Send reply.
+      if (sendReply(socket, request)) {
+        System.out.println("\tReply sent.");
+      } else {
+        System.out.println("\tInvalid message received.");
+      }
+    } // end of while
+  } // end of main
+
+  private static boolean sendReply(DatagramSocket socket,
+                                   DatagramPacket request) throws Exception {
+    InetAddress clientHost = request.getAddress();
+    int clientPort = request.getPort();
+
+    // Read line from received data and split into tokens.
+    byte[] buf = request.getData();
+    String line = getLineFromByteArray(buf);
+    String[] tokens = line.split("\\s+");
+
+    // Make sure is PING message:
+    // PING sequence_number client_send_time passwd CRLF
+    if (!tokens[0].equals("PING") || tokens.length != 4) {
+      return false;
+    }
+
+    String seqNum = tokens[1];
+    String sendTime = tokens[2];
+    String passwd = tokens[3];
+
+    // Make sure passwd is correct.
+    if (!passwd.equals(PASSWD)) {
+      return false;
+    }
+
+    // Build the response:
+    // PINGECHO sequence_number client_send_time passwd CRLF
+    String replyLine = "PINGECHO " + seqNum + " " + sendTime + " " + passwd + " \r\n";
+
+    // Send the reply.
+    byte[] sendData = replyLine.getBytes();
+    DatagramPacket reply = new DatagramPacket(sendData, sendData.length,
+                                              clientHost, clientPort);
+    socket.send(reply);
+
+    return true;
+  }
+
+  private static String getLineFromByteArray(byte[] buf) throws Exception {
+    // Wrap the bytes in a byte array input stream,
+    // so that you can read the data as a stream of bytes.
+    ByteArrayInputStream bais = new ByteArrayInputStream(buf);
+
+    // Wrap the byte array output stream in an input
+    // stream reader, so you can read the data as a
+    // stream of **characters**: reader/writer handles
+    // characters
+    InputStreamReader isr = new InputStreamReader(bais);
+
+    // Wrap the input stream reader in a bufferred reader,
+    // so you can read the character data a line at a time.
+    // (A line is a sequence of chars terminated by any
+    // combination of \r and \n.)
+    BufferedReader br = new BufferedReader(isr);
+
+    // The message data is contained in a single line,
+    // so read this line.
+    String line = br.readLine();
+
+    return line;
+  }
+
+  /*
+  * Print ping data to the standard output stream.
+  */
+  private static void printData(DatagramPacket request) throws Exception {
+    // Obtain references to the packet's array of bytes.
+    byte[] buf = request.getData();
+
+    String line = getLineFromByteArray(buf);
+
+    // Print host address and data received from it.
+    System.out.println("Received from " +
+      request.getAddress().getHostAddress() + ": " +
+      new String(line));
+  } // end of printData
+
+  private static void printUsage() {
+    System.out.println(
+      "usage: java PingServer port passwd [-delay delay] [-loss loss]");
+  }
+
+  private static boolean processArgs(String[] args) {
+    // Get command line argument.
+    if (args.length < 2) {
+      return false;
+    }
+
+    // Check for -delay and -loss flags.
+    boolean isDelay = false;
+    boolean isLoss = false;
+    for (int i = 2; i < args.length; i ++) {
+      if (isDelay) {
+        AVERAGE_DELAY = Integer.parseInt(args[i]);
+        isDelay = false;
+        continue;
+      }
+      if (isLoss) {
+        LOSS_RATE = Integer.parseInt(args[i]);
+        isLoss = false;
+        continue;
+      }
+      if (args[i].equals("-delay")) {
+        isDelay = true;
+      } else if (args[i].equals("-loss")) {
+        isLoss = true;
+      } else {
+        System.out.println("invalid flag " + args[i]);
+        return false;
+      }
+    }
+    if (isDelay || isLoss) {
+      return false;
+    }
+
+    PORT = Integer.parseInt(args[0]);
+    PASSWD = args[1];
+
+    return true;
+  }
+} // end of class
